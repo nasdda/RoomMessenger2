0d044d0dda110e0418b48b8726d8d58a74fe74ea
Xin <xin2001yan@gmail.com>
2021-09-13 20:14:17 -0700

    update chat room

--- client/src/components/pages/chat_room.js
+++ client/src/components/pages/chat_room.js
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { makeStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import TextField from '@material-ui/core/TextField'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+
+import Button from '@material-ui/core/Button'
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) => ({
-    chatArea: {
-        width: "100%",
-    },
     outerContainer: {
-        width: "30rem",
-        height: "30rem",
-        margin: "auto"
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
     },
-    gridItem: {
-        width: "100%",
+    chat: {
+        flex: "1 1 auto",
         backgroundColor: "white",
-        borderRadius: "10px"
+        padding: "1rem",
+        overflowWrap: 'break-word',
+        overflowY: "auto"
+    },
+    input: {
+        flex: "0 1 0",
+        backgroundColor: "white",
+        width: "100%",
+        display: "flex",
+        height: "6rem",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    sendButton: {
+        height: "1rem",
+        width: "1rem",
+        borderRadius: "1rem",
+        fontSize: "1rem",
+        padding: "0.6rem"
+    },
+    textField: {
+        width: "80%",
+        margin: "1rem",
+        height: "1.5rem",
+        borderRadius: "1rem",
+        padding: "0.2rem 0.5rem",
+        outline: "none",
+        fontSize: "1rem",
+        "&:focus": {
+            border: "2px solid #673ab7"
+        }
     }
 }));
 
@@ -24,30 +53,30 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ChatRoom() {
     const classes = useStyles()
+    const [inputText, setInputText] = useState("")
     return (
-        <Grid
-            container
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            spacing={3}
-            className={classes.outerContainer}
-        >
-            <Grid item className={classes.gridItem}>
-                <div>
-                    helloo
-                </div>
-            </Grid>
-            <Grid item className={classes.gridItem}>
-                <TextField
+        <div className={classes.outerContainer}>
+            <div className={classes.chat}>
+
+            </div>
+            <div className={classes.input}>
+                <input
                     placeholder="Message"
-                    variant="outlined"
                     type="text"
-                    size="small"
-                    fullWidth
+                    value={inputText}
+                    onChange={event => { setInputText(event.target.value) }}
+                    className={classes.textField}
                 />
-            </Grid>
-        </Grid>
+                <IconButton 
+                color="primary" 
+                aria-label="Send" 
+                component="span"
+                className={classes.sendButton}
+                >
+                    <SendIcon />
+                </IconButton>
+            </div>
+        </div>
     )
 
 }
